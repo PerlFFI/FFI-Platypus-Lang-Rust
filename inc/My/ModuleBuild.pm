90516e2d39564aac87208b93ce1aca6b612aeac5
Graham Ollis <plicease@cpan.org>
2015-01-29 17:45:21 -0500

    doco and such

--- inc/My/ModuleBuild.pm
+++ inc/My/ModuleBuild.pm
@@ -8,10 +8,14 @@ sub ACTION_dist
 {
   my($self) = @_;
   $self->dispatch('distdir');
-  
   my $dist_dir = $self->dist_dir;
   $self->make_tarball($dist_dir);
   # $self->delete_filetree($dist_dir);
+}
+
+sub ACTION_readme
+{
+  system $^X, 'inc/run/readme.pl';
 }
 
 1;
--- inc/run/readme.pl
+++ inc/run/readme.pl
@@ -6,9 +6,15 @@ use Pod::Simple::Text;
 
 my $root = Pod::Abstract->load_file('lib/FFI/Platypus/Lang/Rust.pm');
 
-$_->detach for $root->select('//#cut');
+#$_->detach for $root->select('//#cut');
 
 pod2txt( $root => 'README' );
+
+foreach my $name (qw( SUPPORT CONTRIBUTING ))
+{
+  my($pod) = $root->select("/head1[\@heading=~{$name}]");
+  pod2txt($pod => $name);
+}
 
 sub pod2txt
 {
