005de184604008d6a852b3758249ce08781d286b
oylenshpeegul <theaney@gmail.com>
2019-04-07 17:50:23 -0400

    Reformat with rustfmt
    And let's end with a bang instead of a newline.

--- examples/string.rs
+++ examples/string.rs
@@ -4,8 +4,7 @@
 // borrowed from:
 // http://doc.rust-lang.org/book/ffi.html
 
-
 #[no_mangle]
 pub extern "C" fn hello_rust() -> *const u8 {
-  "Hello, world\n\0".as_ptr()
+    "Hello, world!\0".as_ptr()
 }
