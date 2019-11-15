64830e3c37054c1865865fe141e71549a0aac306
Graham Ollis <plicease@cpan.org>
2019-11-15 13:59:17 -0500

    update doco and stuff

--- lib/FFI/Build/File/Cargo.pm
+++ lib/FFI/Build/File/Cargo.pm
@@ -7,9 +7,36 @@ use File::chdir;
 use FFI::CheckLib 0.11 qw( find_lib_or_exit );
 use File::Copy qw( copy );
 use Path::Tiny ();
+use FFI::Build::File::Base 1.00 ();
 use base qw( FFI::Build::File::Base );
 use constant default_suffix => '.toml';
 use constant default_encoding => ':utf8';
+
+our $VERSION = '0.06';
+
+=head1 NAME
+
+FFI::Build::File::Cargo - Write Rust extensions for Perl!
+
+=head1 SYNOPSIS
+
+Crete a rust project in the C<ffi> directory that produces a dynamic library:
+
+ nyx% ls -R
+ .:
+ Cargo.lock  Cargo.toml  src/
+ 
+ ./src:
+ lib.rs
+
+Everything else works exactly like C.  See L<FFI::Build> for details.
+
+=head1 DESCRIPTION
+
+This module provides the necessary machinery to bundle rust code with your
+Perl extension.  It uses L<FFI::Build> and C<cargo> to do the heavy lifting.
+
+=cut
 
 sub accept_suffix
 {
@@ -105,3 +132,31 @@ sub _deps
 }
 
 1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<FFI::Platypus>
+
+The Core Platypus documentation.
+
+=item L<FFI::Platypus::Lang::Rust>
+
+Rust language plugin for Platypus.
+
+=back
+
+=head1 AUTHOR
+
+Graham Ollis E<lt>plicease@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
