20dce7a4dabf77a46f75f42af1d711a8d88b876e
Graham Ollis <plicease@cpan.org>
2022-10-23 11:48:11 -0600

    pull the callback example from the examples directory too

--- examples/callback.rs
+++ examples/callback.rs
@@ -11,6 +11,7 @@ pub extern "C" fn rust_log(logf: PerlLog) {
     let lines: [&str; 3] = ["Hello from rust!", "Something else.", "The last log line"];
 
     for line in lines.iter() {
+        // convert string slice to a C style NULL terminated string
         let line = CString::new(*line).unwrap();
         logf(line.as_ptr());
     }
