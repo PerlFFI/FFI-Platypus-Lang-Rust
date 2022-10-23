b891731a26398ec28cafeca84bdc5f670469a7bf
Graham Ollis <plicease@cpan.org>
2022-10-23 16:31:30 -0600

    string argument example in full

---
+++ examples/string/argument.rs
@@ -0,0 +1,17 @@
+#![crate_type = "cdylib"]
+
+use std::ffi::CStr;
+
+#[no_mangle]
+pub extern "C" fn how_many_characters(s: *const i8) -> isize {
+    if s.is_null() {
+        return -1;
+    }
+
+    let s = unsafe { CStr::from_ptr(s) };
+
+    match s.to_str() {
+        Ok(s) => s.chars().count() as isize,
+        Err(_) => -2,
+    }
+}
